4ea46fa6d3c52990a332e490aff0cc99d0fdbd8d
Anzelah <jepkoechanzellah@gmail.com>
2023-05-21 10:41:25 -0700

    execute function

--- execute_action.c
+++ execute_action.c
@@ -6,17 +6,29 @@
  */
 int execute_action(_action action)
 {
-	char *environ[]={"home=/","git=","PATH=/home:bin/"};
+	pid_t childpid, my_pid;
+	int status;
+
+	childpid = fork();
+	if (childpid == -1) /* if forking fails */
+	{
+		perror("Error:");
+		return (1);
+	}
+	else if (childpid == 0) /* in the child process */
+	{
+		if (execve(argv[0], action->args, NULL) == -1)
+			perror("Error:");
+	}
+	else
+	{
+		wait(&status); /* or waitpid(childpid, &status, 0) */
+	}
+	return (0);
+}
+
+	/*char *environ[]={"home=/","git=","PATH=/home:bin/"};
 	pid_t child_pid = fork();
 	if (child_pid == 0)
 	{
 		/*child process*/
-		execve(, action->args, envion);
-		_printf("error");
-	}
-	else
-	{
-		/*main process*/
-	}
-	return (0);
-}
