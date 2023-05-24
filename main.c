9f77bf68453bc6d4ba3f9d450167d3672f004544
Anzelah <jepkoechanzellah@gmail.com>
2023-05-24 03:34:40 -0700

    remove strlen check

--- main.c
+++ main.c
@@ -11,11 +11,10 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 	size_t len = 0, j;
 	char *line = NULL, **parsed_input;
 	ssize_t r_getline = 0;
-	int interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
-		if (interactive)
+		if (isatty(STDIN_FILENO))
 		{
 			_printf("$ ");
 		}
@@ -31,16 +30,11 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 			free(line);
 			continue;
 		}
-		for (j = _strlen(line) - 1; j > 0; j--)/* check for empty spaces. Easier to check from end of file */
+		for (j = _strlen(line) - 1; j > 0; j--)/* check for empty space */
 		{
 			if (line[j] != ' ' && line[j] != '\n' && line[j] != '\t')
 				break;
 		line[j] = '\0';
-		}
-		if (_strlen(line) == 0)
-		{
-			free(line);
-			continue;
 		}
 		parsed_input = parse_input(line);
 		free(line);
