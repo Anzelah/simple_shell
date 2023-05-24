d432b3ab93a74d38ae03c0ce0ce07647ed0f3b18
abdoneverland <ch.abdel.news@gmail.com>
2023-05-24 01:27:55 +0100

    fixing find path

--- _getenv.c
+++ _getenv.c
@@ -49,7 +49,7 @@ int _strncmp(char *s1, char *s2, int n)
 		else if (s1[i] == '\0' && s2[i] == '\0')
 			return (0);
 		i++;
-		if(i >= n)
+		if (i >= n)
 		return (0);
 	}
 }
@@ -65,7 +65,7 @@ char *_getenv(char *nm)
 
 	for (i = 0; environ[i] != NULL; i++)
 	{
-		if (_strncmp(nm, environ[i],_strlen(nm)) == 0)
+		if (_strncmp(nm, environ[i], _strlen(nm)) == 0)
 		{
 			r = _strstr(environ[i], "=");
 			r++;
