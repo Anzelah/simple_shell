788ae9a20b639bc74d044bfc7b88e3077e9cd384
abdoneverland <ch.abdel.news@gmail.com>
2023-05-26 21:05:06 +0100

    handle exit arguments

--- execute_action.c
+++ execute_action.c
@@ -1,6 +1,47 @@
 #include "main.h"
 
 /**
+ * _atoi - convert a string to integer
+ *@s: the string
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+	int len = 0;
+	int sign = 1;
+	unsigned int result = 0;
+	unsigned int ten = 1;
+	int otherchar = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	len--;
+	while (len >= 0)
+	{
+		if (s[len] >= '0' && s[len] <= '9')
+		{
+			if (otherchar && result != 0)
+			{
+				result = 0;
+				ten = 1;
+				sign = 1;
+			}
+
+			result += (s[len] - '0') * ten;
+			ten *= 10;
+
+			otherchar = 0;
+		}
+		else
+			otherchar = 1;
+		if (s[len] == '-')
+			sign *= -1;
+		len--;
+	}
+	return (sign * result);
+}
+/**
  * execute_action - executes action
  *@action: the action to execute
  * Return: 0 to exit or 1
@@ -12,7 +53,12 @@ int execute_action(char **action)
 	char *path = NULL;
 
 	if (_strcmp(action[0], "exit") == 0)
-		return (0);
+	{
+		if (action[1] == NULL)
+			return (0);
+		else
+			exit(_atoi(action[1]));
+	}
 	if (_strcmp(action[0], "cd") == 0)
 		builtin_cd(action);
 	if (_strcmp(action[0], "env") == 0)
