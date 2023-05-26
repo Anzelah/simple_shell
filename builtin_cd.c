85e2b7a7069909f09e590c7888c54ddb70ca42c6
abdoneverland <ch.abdel.news@gmail.com>
2023-05-26 19:51:06 +0100

    add cd

--- builtin_cd.c
+++ builtin_cd.c
@@ -4,21 +4,36 @@
 void builtin_cd(char **args)
 {
 	char *home;
+	char temp[1024];
 	if (!args[1])
 	{
 		home = _getenv("HOME");
 		if (home)
+		{
+			if (getcwd(temp, sizeof(temp)) == NULL)
+				return;
+			setenv("OLDPWD",temp, 1);
 			chdir(home);
+		}
 		else
 			perror("");
 	}
+	else if (_strcmp(args[1], "-") == 0)
+	{
+		if (_getenv("OLDPWD"))
+			chdir(_getenv("OLDPWD"));
+	}
 	else
 	{
+		if (getcwd(temp, sizeof(temp)) == NULL)
+			return;
+		setenv("OLDPWD",temp, 1);
 		if (chdir(args[1]) != 0)
 		{
 			perror("");
 			return;
 		}
+		setenv("OLDPWD",temp, 1);
 		setenv("PWD", args[1], 1);
 	}
 }
